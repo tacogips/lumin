e8e6120530ae6f5c6c834732c58ba0dc7119b916
tacogips <me@tacogips.me>
2025-05-21 10:31:28 +0900

    update

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
         # Simple package definition
         packages.default = buildRustPackageCustom {
           pname = "lumin";
-          version = "0.1.7";
+          version = "0.1.8";
           src = ./.;
 
           # Basic cargo lock configuration
